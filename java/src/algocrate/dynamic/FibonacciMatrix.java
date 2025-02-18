78a772ca7ca74486790acfeaa730d4fa47f7d5af
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-18 19:17:05 +0530

    documentation

--- java/src/algocrate/dynamic/FibonacciMatrix.java
+++ java/src/algocrate/dynamic/FibonacciMatrix.java
@@ -1,5 +1,9 @@
 package algocrate.dynamic;
 
+/*
+ * Returns the nth fibonacci number, provided integer n, using Matrix Multiplication approach. 
+ * Time complexity : O(log(n)), Space : O(1), highly efficient algo. 
+ */
 public class FibonacciMatrix {
 
 }
