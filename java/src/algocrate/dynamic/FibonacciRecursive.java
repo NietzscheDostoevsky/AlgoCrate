9adef0280ea31e13ed8815fc494d12500dbced40
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-18 19:13:15 +0530

    added timing code and documentation

--- java/src/algocrate/dynamic/FibonacciRecursive.java
+++ java/src/algocrate/dynamic/FibonacciRecursive.java
@@ -1,7 +1,9 @@
 package algocrate.dynamic;
+import algocrate.utils.Stopwatch;
 
 /*
  * Returns the nth fibonacci number, provided integer n, using recursive approach. 
+ * Time Complexity of 2^n, exponential 
  */
 public class FibonacciRecursive {
 	
@@ -15,5 +17,16 @@ public class FibonacciRecursive {
 	public static void main(String[] args) {
 		int n = 10;
 		System.out.print("Fibonacci number at position " + n + " is : " + fibonacci(n));
+		
+		Stopwatch stopwatch = new Stopwatch();
+		
+		// calculating time for 40 runs
+		System.out.println("First hundred digits of fibonacci sequence are : ");
+		for(int i = 1; i < 50; i++) {
+			System.out.print(fibonacci(i) + " ");
+			if (i % 10 == 0) System.out.println();
+		}
+		
+		System.out.println("Time elapsed : " + stopwatch.elapsedTime() + " Seconds");
 	}
 }
