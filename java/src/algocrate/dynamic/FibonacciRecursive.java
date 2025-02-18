377334da84c5192888d6ed1f95d3b6ccb7785d6c
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-18 18:53:24 +0530

    recursive approach for fibonacci

--- java/src/algocrate/dynamic/FibonacciRecursive.java
+++ java/src/algocrate/dynamic/FibonacciRecursive.java
@@ -1,5 +1,19 @@
 package algocrate.dynamic;
 
+/*
+ * Returns the nth fibonacci number, provided integer n, using recursive approach. 
+ */
 public class FibonacciRecursive {
-
+	
+	public static int fibonacci(int n) {
+		if (n <= 1) {
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	
+	public static void main(String[] args) {
+		int n = 10;
+		System.out.print("Fibonacci number at position " + n + " is : " + fibonacci(n));
+	}
 }
