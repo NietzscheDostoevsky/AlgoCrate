e2c9b37811e7442dc7a850cf63c8022f5c288af5
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-19 21:03:28 +0530

    reformat and complete

--- java/src/algocrate/dataStructures/Bag.java
+++ java/src/algocrate/dataStructures/Bag.java
@@ -19,12 +19,12 @@
  * 
  ******************************************************************************/
 
-
 package algocrate.dataStructures;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
+
+import algocrate.utils.StdIn;
 
 /*
  * The Bag class represents a bag ( or multiset ) of generic items. 
@@ -37,111 +37,90 @@ import java.util.Scanner;
 
 public class Bag<Item> implements Iterable<Item> {
 	private Node<Item> first; // begining of bag
-	private int n; 			  // number of elements in bag
-	
+	private int n; // number of elements in bag
+
 	// helper linked list class
 	private static class Node<Item> {
-		private Item item;        //Store the data
-		private Node<Item> next;  //Pointer to the next node
+		private Item item; // Store the data
+		private Node<Item> next; // Pointer to the next node
 	}
-	
+
 	/*
 	 * Initialize an empty bag
 	 */
 	public Bag() {
 		first = null;
-		n = 0; 
+		n = 0;
 	}
-	
+
 	public boolean isEmpty() {
-		return first == null; 
+		return first == null;
 	}
-	
+
 	public int size() {
 		return n;
 	}
-	
+
 	/*
 	 * Add the Item to this bag
 	 */
 	public void add(Item item) {
-		Node<Item> oldfirst = first; 
-		first = new Node<Item>(); 
-		first.item = item; 
+		Node<Item> oldfirst = first;
+		first = new Node<Item>();
+		first.item = item;
 		first.next = oldfirst;
 		n++;
 	}
-	
+
 	/*
-	 * Returns an interator that interates over the items in this bag in arbitrary order.
-	 * Since it's being implemented using a LinkedList, the iterator order should be reverse of addition to the bag
+	 * Returns an interator that interates over the items in this bag in arbitrary
+	 * order. Since it's being implemented using a LinkedList, the iterator order
+	 * should be reverse of addition to the bag
 	 */
 	public Iterator<Item> iterator() {
 		return new LinkedIterator(first);
 	}
-	
-	//	Doesn't support remove operation, for now
+
+	// Doesn't support remove operation, for now
 	private class LinkedIterator implements Iterator<Item> {
 		private Node<Item> current;
-		
+
 		public LinkedIterator(Node<Item> first) {
-			current = first; 
+			current = first;
 		}
-		
+
 		public boolean hasNext() {
 			return current != null;
 		}
-		
+
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
-		
+
 		public Item next() {
-			if (!hasNext()) throw new NoSuchElementException();
+			if (!hasNext())
+				throw new NoSuchElementException();
 			Item item = current.item;
-			current = current.next; 
+			current = current.next;
 			return item;
 		}
 	}
-	
+
 	/*
-	 * Unit test for bag data type. 
+	 * Unit test for bag data type.
 	 */
 	public static void main(String[] args) {
+
 		Bag<String> bag = new Bag<String>();
-		Scanner scanner = new Scanner(System.in);
-		
-		
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			bag.add(item);
+		}
+
+		System.out.println("size of bag = " + bag.size());
+		for (String s : bag) {
+			System.out.println(s);
+		}
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
