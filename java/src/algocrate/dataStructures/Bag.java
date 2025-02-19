4f78b2cb463fc8bfeb0e8fff4817e9a606b8474c
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-19 17:43:22 +0530

    added documentations

--- java/src/algocrate/dataStructures/Bag.java
+++ java/src/algocrate/dataStructures/Bag.java
@@ -1,3 +1,25 @@
+/******************************************************************************
+ * 
+ * A generic bag data structure, implemented using a singly linked list. 
+ * 
+ * Usage : 
+ * % cat queen.txt 
+ *   is this the real life is this just fantasy
+ * % java Bag < test.txt
+ * size of bag = 9
+ * fantasy
+ * just
+ * this
+ * is
+ * life
+ * real
+ * the
+ * this
+ * is
+ * 
+ ******************************************************************************/
+
+
 package algocrate.dataStructures;
 
 public class Bag {
