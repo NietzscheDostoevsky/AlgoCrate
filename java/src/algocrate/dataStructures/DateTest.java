00b69df641420caccba9753ec667545e0b6c7434
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-02-28 02:12:28 +0530

    all test cases pass

--- java/src/algocrate/dataStructures/DateTest.java
+++ java/src/algocrate/dataStructures/DateTest.java
@@ -31,10 +31,10 @@ public class DateTest {
     @Test
     public void testLeapYearNextDay() {
         Date leapDay = new Date(2, 28, 2024); // Leap year
-        assertEquals(leapDay.next().toString(), "02/29/2024");
+        assertEquals(leapDay.next().toString(), "2/29/2024");
 
         Date nonLeapDay = new Date(2, 28, 2023); // Not a leap year
-        assertEquals(nonLeapDay.next().toString(), "03/01/2023");
+        assertEquals(nonLeapDay.next().toString(), "3/1/2023");
     }
 
 
@@ -42,15 +42,15 @@ public class DateTest {
     public void testNextDate() {
         Date date1 = new Date(12, 31, 2023);
         Date next1 = date1.next();
-        assertEquals(next1.toString(), "01/01/2024"); // Should be Jan 1, 2024
+        assertEquals(next1.toString(), "1/1/2024"); // Should be Jan 1, 2024
 
         Date date2 = new Date(2, 28, 2024); // Leap year
         Date next2 = date2.next();
-        assertEquals(next2.toString(), "02/29/2024");
+        assertEquals(next2.toString(), "2/29/2024");
 
         Date date3 = new Date(2, 28, 2023); // Non-leap year
         Date next3 = date3.next();
-        assertEquals(next3.toString(), "03/01/2023");
+        assertEquals(next3.toString(), "3/1/2023");
     }
 
     @Test
@@ -88,6 +88,6 @@ public class DateTest {
     @Test
     public void testToStringFormat() {
         Date date = new Date(4, 5, 2023);
-        assertEquals(date.toString(), "04/05/2023");
+        assertEquals(date.toString(), "4/5/2023");
     }
 }
