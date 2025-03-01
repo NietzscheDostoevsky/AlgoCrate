5144110e2fe8c97aedbeca7d2bb8f81fc62c733d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-01 23:24:21 +0530

    constructor

--- java/src/algocrate/dataStructures/MinPQ.java
+++ java/src/algocrate/dataStructures/MinPQ.java
@@ -9,3 +9,54 @@
  * 
  ******************************************************************************/
 
+package algocrate.dataStructures;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class MinPQ<Key> implements Iterable<Key> {
+
+	private Key[] pq; 					// stores indices from 1 to N, 0 being empty for calculations in heap.
+	private int N; 						// number of items on the PQ
+	private Comparator<Key> comparator; // comparator 
+	
+	// Constructor for an empty PQ
+	@SuppressWarnings("unchecked")
+	public MinPQ(int initialCapacity) {
+		pq = (Key[]) new Object[initialCapacity + 1]; // 0th index to be left emtpy
+		N = 0;
+	}
+	
+	
+	@Override
+	public Iterator<Key> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
