80bc1126c8131d45e34830a7d489ba151a0885f5
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-01 20:22:25 +0530

    documentation

---
+++ java/src/algocrate/dataStructures/MinPQ.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * The MinPQ class represents a priority queue of generic keys. It supports 
+ * 		- insert
+ * 		- delete the minimum
+ * operations in O(logN) , N being the number of elements in the PQ
+ * 
+ * The implementation uses a Binary Heap data Structure to achieve this. 
+ * 
+ * 
+ ******************************************************************************/
+
