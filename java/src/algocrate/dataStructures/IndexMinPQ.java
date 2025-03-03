7aabb707e12fb2e0e11812544c156f899f81fced
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-03 15:31:44 +0530

    basic structure of the class

--- java/src/algocrate/dataStructures/IndexMinPQ.java
+++ java/src/algocrate/dataStructures/IndexMinPQ.java
@@ -36,57 +36,148 @@ import java.util.NoSuchElementException;
 public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> { //Key being generic, should be comaparable
 																					//Iterable<Integer> ensures PQ itself can be iterated over, returning indices(not keys)
 	
-	private int maxN;		//Maximum number of elements on PQ
+	private int maxN;		//Maximum number of elements on PQ, not using dynamic resizing.
 	private int n;			//number of elements on PQ
 	private int[] pq;		//binary heap using 1 based indexing
 	private int[] qp;		//inverse of pq -> qp[pq[i]] = pq[qp[i]] = 1
 	private Key[] keys; 	//keys[i] = priority of i
 	
-	@Override
-	public Iterator<Integer> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	// Constructor for an empty PQ with fixed initial capacity of maxN
+	@SuppressWarnings("unchecked")
+	public IndexMinPQ(int maxN) {
+		if (maxN < 0) throw new IllegalArgumentException();
+		this.maxN = maxN;
+		n = 0;
+		keys = (Key[]) new Comparable[maxN + 1];
+		pq = new int[maxN + 1];
+		qp = new int[maxN + 1];
+		
+		for (int i = 0; i <= maxN; i++)
+			qp[i] = -1; 					//signifies empty index		
 	}
 	
+	// returns true if this pq is empty 
+	public boolean isEmpty() {
+		return n == 0;
+	}
+	
+	// Is index i on this pq? 
+	public boolean contains(int i) {
+		
+	}
+	
+	// Returns the number of keys on this pq
+	public int size() {
+		
+	}
+	
+	// Associates key with index i
+	public void insert(int i, Key key) {
+		
+	}
+	
+	// Returns an index associated with a minimum key
+	public int minIndex() {
+		
+	}
+	
+	// Returns a minimum key. 
+	public Key minKey() {
+		
+	}
+	
+	// Removes a minium key and returns its associated index.
+	public int delMin() {
+		
+	}
+	
+	// Returns the key assocaited with index i
+	public Key keyOf(int i) {
+		
+	}
+	
+	// Change the key associated with index i to the specified value. 
+	public void changeKey(int i, Key key) {
+		
+	}
+	
+	// Decreaase the key associated with index i to the specified value. 
+	public void decreaseKey(int i, Key key) {
+		
+	}
+	
+	// Increase the key associated with index i to the specified value. 
+	public void increaseKey(int i, Key key) {
+		
+	}
+	
+	// Remove the key associated with index i
+	public void delete(int i) {
+		
+	}
+	
+	// throw an IllegalArgumentException if i is an invalid index
+	private void validateIndex(int i) {
+		
+	}
+	
+	/***************************************************************************
+	 * General helper functions.
+	 ***************************************************************************/
+	
+	// is key i greater than j
+	private boolean greater(int i, int j) {
+		
+	}
+	
+	// exchange values at index i and j
+	private void exch(int i, int j) {
+		
+	}
+	
+	/***************************************************************************
+	 * Heap helper functions.
+	 ***************************************************************************/
+	
+	// Swim a node from bottom to up 
+	private void swim(int k) {
+		
+	}
+	
+	// Sink a node from top to bottom
+	private void sink(int k) {
+		
+	}
+	
+	/***************************************************************************
+	* Iterator
+	***************************************************************************/
+	
+	// Returns an iterator that iterates over the keys on the pq in "ascending order"
+	//		Doesn't implement remove()
+	
+	@Override
+	public Iterator<Integer> iterator() {
+		return new HeapIterator();
+	}
+	
+	private class HeapIterator implements Iterator<Integer> {
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public Integer next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public void remove() {
+			
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
