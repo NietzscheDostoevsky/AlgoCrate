61b7f404c4f0ce842a41caf8aafd0836c171f78d
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-05 18:19:09 +0530

    basic functionalities

--- java/src/algocrate/dataStructures/IndexMinPQ.java
+++ java/src/algocrate/dataStructures/IndexMinPQ.java
@@ -64,62 +64,102 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 	
 	// Is index i on this pq? 
 	public boolean contains(int i) {
-		
+		validateIndex(i);
+		return qp[i] != -1; 				//By definition
 	}
 	
 	// Returns the number of keys on this pq
 	public int size() {
-		
+		return n;
 	}
 	
 	// Associates key with index i
 	public void insert(int i, Key key) {
-		
+		validateIndex(i);
+		if (contains(i)) throw new IllegalArgumentException("index is already on the PQ");
+		n++;
+		qp[i] = n;
+		pq[n] = i;
+		keys[i] = key;
+		swim(n);
 	}
 	
 	// Returns an index associated with a minimum key
 	public int minIndex() {
-		
+		if (n == 0) throw new NoSuchElementException("Priority queue underflow");
+		return pq[1];
 	}
 	
 	// Returns a minimum key. 
 	public Key minKey() {
-		
+		return keys[minIndex()];
 	}
 	
-	// Removes a minium key and returns its associated index.
+	// Removes a minimum key and returns its associated index.
 	public int delMin() {
-		
+		if (n == 0) throw new NoSuchElementException("Priority queue underflow");
+		int min = pq[1];
+		exch(1, n--); 				// put the last node at the top
+		sink(1); 	  				// restore heap
+		assert min == pq[n + 1];	// sanity check for n
+		qp[min] = -1;				// delete
+		keys[min] = null;			// avoid lingering
+		pq[n + 1] = -1;				// not needed anymore
+		return min;
 	}
 	
 	// Returns the key assocaited with index i
 	public Key keyOf(int i) {
+		if (!contains(i)) throw new NoSuchElementException("index not in pq");
+		else return keys[i];
 		
 	}
 	
 	// Change the key associated with index i to the specified value. 
 	public void changeKey(int i, Key key) {
-		
+		if (!contains(i)) throw new NoSuchElementException("index not on pq");
+		keys[i] = key;
+		swim(qp[i]);
+		sink(qp[i]);
 	}
 	
 	// Decreaase the key associated with index i to the specified value. 
 	public void decreaseKey(int i, Key key) {
-		
+		if (!contains(i)) throw new NoSuchElementException("index not on the PQ");
+		if (keys[i].compareTo(key) == 0)
+			throw new IllegalArgumentException("calling decreaseKey() with a key equal to the key in the PQ");
+		if (keys[i].compareTo(key) < 0) 
+			throw new IllegalArgumentException("calling decreaseKey() with a key strictly greater than the key in the PQ");
+		keys[i] = key;
+		swim(qp[i]);
 	}
 	
 	// Increase the key associated with index i to the specified value. 
 	public void increaseKey(int i, Key key) {
-		
+		if (!contains(i)) throw new NoSuchElementException("index not on the PQ");
+		if (keys[i].compareTo(key) == 0)
+			throw new IllegalArgumentException("calling increaseKey() with a key equal to the key in the PQ");
+		if (keys[i].compareTo(key) > 0) 
+			throw new IllegalArgumentException("calling increaseKey() with a key strictly less than the key in the PQ");
+		keys[i] = key;
+		sink(qp[i]);
 	}
 	
 	// Remove the key associated with index i
 	public void delete(int i) {
-		
+		if (!contains(i)) throw new NoSuchElementException("index not on the PQ");
+		int index = qp[i];
+		exch(index, n--);
+		swim(index);
+		sink(index);
+		keys[i] = null;
+		qp[i] = -1;
 	}
 	
 	// throw an IllegalArgumentException if i is an invalid index
 	private void validateIndex(int i) {
-		
+		if (i < 0) throw new IllegalArgumentException("negative index : " + i);
+		if (i >= maxN) throw new IllegalArgumentException("index >= capacity: " + i);
 	}
 	
 	/***************************************************************************
@@ -128,12 +168,16 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 	
 	// is key i greater than j
 	private boolean greater(int i, int j) {
-		
+		return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
 	}
 	
 	// exchange values at index i and j
 	private void exch(int i, int j) {
-		
+		int temp = pq[i];
+		pq[i]= pq[j];
+		pq[j] = temp;
+		qp[pq[i]] = i;
+		qp[pq[j]] = j;
 	}
 	
 	/***************************************************************************
