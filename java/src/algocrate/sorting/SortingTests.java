87ce14b5c91aa8ab090f9994d33bf1150586e4d9
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-07 12:08:17 +0530

    common testing class for sorting algos

---
+++ java/src/algocrate/sorting/SortingTests.java
@@ -0,0 +1,57 @@
+package algocrate.sorting;
+
+package algocrate.utils;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import java.util.Arrays;
+
+import algocrate.sorting.*; // Import your sorting algorithm classes
+
+public class SortingTests {
+
+    @DataProvider(name = "sortingData")
+    public Object[][] sortingData() {
+        return new Object[][] {
+            {new Integer[]{5, 2, 9, 1, 5, 6}},
+            {new Integer[]{3, -1, 4, 1, 5, 9, 2, 6, 5}},
+            {new Integer[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}},
+            {new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+            {new Integer[]{42}}, // Single element
+            {new Integer[]{}}, // Empty array
+        };
+    }
+
+//    @Test(dataProvider = "sortingData")
+//    public void testMergeSort(Integer[] input) {
+//        Integer[] expected = input.clone();
+//        Arrays.sort(expected);
+//        MergeSort.sort(input);
+//        Assert.assertEquals(input, expected, "MergeSort failed");
+//    }
+//
+//    @Test(dataProvider = "sortingData")
+//    public void testSelectionSort(Integer[] input) {
+//        Integer[] expected = input.clone();
+//        Arrays.sort(expected);
+//        SelectionSort.sort(input);
+//        Assert.assertEquals(input, expected, "SelectionSort failed");
+//    }
+//
+//    @Test(dataProvider = "sortingData")
+//    public void testQuickSort(Integer[] input) {
+//        Integer[] expected = input.clone();
+//        Arrays.sort(expected);
+//        QuickSort.sort(input);
+//        Assert.assertEquals(input, expected, "QuickSort failed");
+//    }
+
+    @Test(dataProvider = "sortingData")
+    public void testHeapSort(Integer[] input) {
+        Integer[] expected = input.clone();
+        Arrays.sort(expected);
+        Heap.sort(input);
+        Assert.assertEquals(input, expected, "HeapSort failed");
+    }
+}
