7b35968f58f7e49223b3339497a40fac6ae6b316
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-07 12:21:38 +0530

    Struture of class

--- java/src/algocrate/sorting/Heap.java
+++ java/src/algocrate/sorting/Heap.java
@@ -1,5 +1,44 @@
+/******************************************************************************
+ * Sorting an 0-index array using HeapSort algorithm. 
+ * 
+ * The class Heap provides a static method sort() to sort an array using heapsort
+ * The sorting algorithm is NOT stable. 
+ *
+ * This implementation takes time O(log n) to sort.
+ * This implementation takes O(1) extra space.  
+ ******************************************************************************/
+
 package algocrate.sorting;
 
 public class Heap {
+	
+	// This class cannote be instantiated.
+	private Heap() {
+	}
 
+	// Rearrange the array in ascending otder, using natural order of the
+	// elements/keys.
+	public static void sort(Comparable[] pq) {
+
+	}
+
+	/***************************************************************************
+	 * Helper functions to restore the heap invariant.
+	 ***************************************************************************/
+
+	private static void sink(Comparable[] pq, int k, int n) {
+
+	}
+
+	/***************************************************************************
+	 * Helper functions for comparisons and swaps. Indices are "off-by-one" to
+	 * support 1-based indexing.
+	 ***************************************************************************/
+	private static boolean less(Comparable[] pq, int i, int j) {
+
+	}
+
+	private static void exch(Object[] pq, int i, int j) {
+	}
+	
 }
