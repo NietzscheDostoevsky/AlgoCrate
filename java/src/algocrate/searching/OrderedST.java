78987ac24c7ddb2d1759d55a6418dd6373a85a1b
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-10 11:01:25 +0530

    reformated for better comment view

--- java/src/algocrate/searching/OrderedST.java
+++ java/src/algocrate/searching/OrderedST.java
@@ -3,38 +3,25 @@ package algocrate.searching;
 // Interface for an Ordered Symbol Table abstract DataType. 
 public interface OrderedST<Key extends Comparable<Key>, Value> {
 	
-	void put(Key key, Value value); 	// put key-value pair into the table, remove key from table if value is null
-	Value get(Key key); 				//value paired with key, null if key is absent
-	void delete(Key key); 				//remove key and its value from table
-	boolean contains(Key key); 			// is there a value paired with key?
-	boolean isEmpty();					// is the table empty? 
-	int size(); 						// number of key-value pairs
-	Key min();							// smallest key
-	Key max();							// largest key
-	Key floor(Key key);					// largest key less than or equal to key
-	Key ceiling(Key key);				// smallest key greater than or equal to key
-	int rank(Key key);					// number of keys less than key
-	Key select(int k);					// key of rank k
-	
-	default void deleteMin() {			// delete smallest key
-		delete(min());
-	}
-	
-	default void deleteMax() {			// delete largest key
-		delete(max());
-	}
-	
-	default int size(Key lo, Key hi) {	// number of keys in [lo...hi]
-		if (hi.compareTo(lo) < 0) 
-			return 0;
-		else if (contains(hi)) 
-			return rank(hi) - rank(lo) + 1;
+	void put(Key key, Value value); 								// put key-value pair into the table, remove key from table if value is null
+	Value get(Key key); 											//value paired with key, null if key is absent
+	void delete(Key key); 											//remove key and its value from table
+	boolean contains(Key key); 										// is there a value paired with key?
+	boolean isEmpty();												// is the table empty? 
+	int size(); 													// number of key-value pairs
+	Key min();														// smallest key
+	Key max();														// largest key
+	Key floor(Key key);												// largest key less than or equal to key
+	Key ceiling(Key key);											// smallest key greater than or equal to key
+	int rank(Key key);												// number of keys less than key
+	Key select(int k);												// key of rank k
+	default void deleteMin() { delete(min()); }						// delete smallest key
+	default void deleteMax()  {	delete(max()); }					// delete largest key
+	default int size(Key lo, Key hi) {								// number of keys in [lo...hi]
+		if (hi.compareTo(lo) < 0) return 0;
+		else if (contains(hi)) return rank(hi) - rank(lo) + 1;
 		else return rank(hi) - rank(lo);
 	}
-	
-	Iterable<Key> keys(Key lo, Key hi); // keys in [lo...hi] in sorted order
-	
-	default Iterable<Key> keys() {		// all keys in the table, in sorted order
-		return keys(min(), max());
-	}
+	Iterable<Key> keys(Key lo, Key hi); 							// keys in [lo...hi] in sorted order
+	default Iterable<Key> keys() {	return keys(min(), max()); }	// all keys in the table, in sorted order	
 }
