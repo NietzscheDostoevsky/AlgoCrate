7f05c8c8a876226310d8152dff725932305fd4ae
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2025-03-10 13:13:22 +0530

    basic test case for whole symbol table as whole

--- java/src/algocrate/searching/SymbolTableTest.java
+++ java/src/algocrate/searching/SymbolTableTest.java
@@ -1,9 +1,10 @@
-package algocrate.searching.tests;
+package algocrate.searching;
 
-import algocrate.searching.SymbolTable;
-import algocrate.searching.BinarySearchTreeST;  // Example implementation
-import algocrate.searching.SeparateChainingHashST;  // Example HashTable
-import algocrate.searching.OrderedST;
+/*
+ * Basic test case Symbol Table API which every implementation of symbol table must pass. 
+ * Only tests two basic functionalities of put() and get() methods
+ */
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -23,51 +24,55 @@ public class SymbolTableTest {
     public void testPutAndGet(SymbolTable<String, Integer> st) {
         st.put("apple", 1);
         st.put("banana", 2);
-        
+
         Assert.assertEquals(st.get("apple"), Integer.valueOf(1));
         Assert.assertEquals(st.get("banana"), Integer.valueOf(2));
-        Assert.assertNull(st.get("cherry"));  // Not inserted, should be null
+        Assert.assertNull(st.get("cherry"));  // Not inserted, should return null
     }
 
     @Test(dataProvider = "symbolTableImplementations")
-    public void testContains(SymbolTable<String, Integer> st) {
-        st.put("dog", 10);
-        Assert.assertTrue(st.contains("dog"));
-        Assert.assertFalse(st.contains("cat"));
+    public void testOverwriteValue(SymbolTable<String, Integer> st) {
+        st.put("key1", 10);
+        Assert.assertEquals(st.get("key1"), Integer.valueOf(10));
+
+        st.put("key1", 20);  // Overwrite existing value
+        Assert.assertEquals(st.get("key1"), Integer.valueOf(20));
+
+        st.put("key1", 30);
+        Assert.assertEquals(st.get("key1"), Integer.valueOf(30));
     }
 
     @Test(dataProvider = "symbolTableImplementations")
-    public void testDelete(SymbolTable<String, Integer> st) {
-        st.put("x", 100);
-        Assert.assertTrue(st.contains("x"));
-
-        st.delete("x");
-        Assert.assertFalse(st.contains("x"));
-        Assert.assertNull(st.get("x"));
+    public void testGetNonExistentKey(SymbolTable<String, Integer> st) {
+        Assert.assertNull(st.get("unknown"));
     }
 
     @Test(dataProvider = "symbolTableImplementations")
-    public void testSize(SymbolTable<String, Integer> st) {
-        Assert.assertEquals(st.size(), 0);
+    public void testPutNullValueRemovesKey(SymbolTable<String, Integer> st) {
+        st.put("test", 100);
+        Assert.assertEquals(st.get("test"), Integer.valueOf(100));
 
-        st.put("one", 1);
-        st.put("two", 2);
-        Assert.assertEquals(st.size(), 2);
-
-        st.delete("one");
-        Assert.assertEquals(st.size(), 1);
+        st.put("test", null); // Should remove the key
+        Assert.assertNull(st.get("test"));
     }
 
     @Test(dataProvider = "symbolTableImplementations")
-    public void testMinMaxIfOrdered(SymbolTable<String, Integer> st) {
-        if (st instanceof OrderedST) {
-            OrderedST<String, Integer> orderedST = (OrderedST<String, Integer>) st;
-            orderedST.put("a", 1);
-            orderedST.put("c", 3);
-            orderedST.put("b", 2);
+    public void testMultiplePutsAndGets(SymbolTable<String, Integer> st) {
+        st.put("a", 1);
+        st.put("b", 2);
+        st.put("c", 3);
 
-            Assert.assertEquals(orderedST.min(), "a");
-            Assert.assertEquals(orderedST.max(), "c");
-        }
+        Assert.assertEquals(st.get("a"), Integer.valueOf(1));
+        Assert.assertEquals(st.get("b"), Integer.valueOf(2));
+        Assert.assertEquals(st.get("c"), Integer.valueOf(3));
+
+        // Overwriting existing values
+        st.put("a", 10);
+        st.put("b", 20);
+        st.put("c", 30);
+
+        Assert.assertEquals(st.get("a"), Integer.valueOf(10));
+        Assert.assertEquals(st.get("b"), Integer.valueOf(20));
+        Assert.assertEquals(st.get("c"), Integer.valueOf(30));
     }
 }
